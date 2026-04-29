b3114b8d373ac99b4b92ae2732b060ce4a895751
Matheus Ribeiro <contato.matheusrbr11@gmail.com>
2026-04-29 09:21:19 -0300

    feat: adicionado autoatualização das bibliotecas antes da execução do programa

--- exe.py
+++ exe.py
@@ -2,4 +2,5 @@ import subprocess
 
 ### ESTE ARQUIVO É NECESSÁRIO PARA QUE POSSAMOS EDITAR O SCRIPT PRINCIPAL SEM COMPROMETER O FUNCIONAMENTO DO EXECUTÁVEL ###
 
-subprocess.run(["python", "//cifs-zone1/tesouro/Programas da SUPCONC/Minerva/main.py"])
+subprocess.run(['//cifs-zone1/tesouro/Programas da SUPCONC/atualizar_jupiter.bat'], check=True, shell=True)
+subprocess.run(["python", "//cifs-zone1/tesouro/Programas da SUPCONC/Programa Minerva/main.py"])
